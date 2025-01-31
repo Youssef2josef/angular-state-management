c6dae88322bd39d6be4cb3a369a2cbccab161eda
jouini9Youssef <jouini9youssef@gmail.com>
2025-01-31 08:47:51 +0100

    Installation of libraries & dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideAnimations(), provideStore(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
 };
